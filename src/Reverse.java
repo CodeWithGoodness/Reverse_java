8e9df507c9fa48c54740d0554a9914bbc356855c
goodness9 <ceciliaokoro2@gmail.com>
2022-03-09 22:26:32 -0800

    modified to also check for palindromes

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,16 +1,22 @@
  import java.util.Scanner;
 public class Reverse{
     public static void main(String[]args){
-        //This program reverses a string
-        Scanner input = new Scanner(System.in);
+        //This program reverses a string and also checks if it's a palindrome
+        Scanner input = new Scanner(System.in); //taking user input
         System.out.println("Enter a word: ");
         String word = input.next();
-        String reversed_array = "";
+        String reversed_array = ""; //stores the reversed array
         //i is arrayindex
 
         for(int i=0; i< word.length(); i++){
             reversed_array = word.charAt(i) + reversed_array;}
         System.out.println(reversed_array);
+        //checking if it's a palindrome
+     if(word.equals(reversed_array))   {
+         System.out.println("The word is a palindrome");
+     }
+     else{
+     System.out.println("Not a palindrome");}
         }
 
 
